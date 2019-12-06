991d63ff649a8faacd307a88565077e839f9c209
leomed2005 <46194955+leomed2005@users.noreply.github.com>
2019-12-06 11:25:54 -0300

    se genera nuevo archivo de eliminacion de versiones antiguas

---
+++ PS/del2.ps1
@@ -0,0 +1,20 @@
+$Server2 = (Get-Content SrvB.txt)
+$NumDays = 7
+$CurDate = get-date
+$TestDate = $Curdate.AddDays(-$NumDays)
+
+$strData = "Files deleted from: " + $Server2 + " on " + $CurDate     
+
+get-childitem -path $Server2 -recurse | foreach { 
+    If ($_.LastWriteTime -lt $TestDate) 
+    { 
+        $strData = "File to delete: " + $_.FullName + " " + $_.LastWriteTime 
+        
+        $strData | out-file "DeletedFiles.log" -Append
+        If ($_.GetType().Name -eq "FileInfo") {
+            $_.Delete()
+        } else {
+            $_.Delete($true)
+        }
+   }
+}
